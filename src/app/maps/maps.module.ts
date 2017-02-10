c7bcf8336d31f1d7fd921f2820ad50b48c2fb10c
Kim Kern <kim.kern@fu-berlin.de>
2017-02-10 19:47:51 +0100

    add maps module

---
+++ src/app/maps/maps.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddressSelectorComponent } from './address-selector/address-selector.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AddressSelectorComponent]
+})
+export class MapsModule { }
