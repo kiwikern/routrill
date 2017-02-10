d0e76f273472a6fb9c09f683262d7732ded21d6d
Angular CLI <angular-cli@angular.io>
2017-02-10 19:05:51 +0100

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { TspPage } from './app.po';
+
+describe('tsp App', function() {
+  let page: TspPage;
+
+  beforeEach(() => {
+    page = new TspPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
