8bd47fc2d33e761a5133b20658ef9b322fe3c606
Kim Kern <kim.kern@fu-berlin.de>
2017-02-10 19:50:28 +0100

    add address selector route

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddressSelectorComponent } from './address-selector/address-selector.component';
+import {Routes, RouterModule} from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'select-address',
+    component: AddressSelectorComponent
+  }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
   declarations: [AddressSelectorComponent]
 })
