aa7386a75b72f69139ca4f915d71fa57471a31a4
Kim Kern <kim.kern@fu-berlin.de>
2017-02-10 19:48:08 +0100

    add address selector component

---
+++ src/app/maps/address-selector/address-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-address-selector',
+  templateUrl: './address-selector.component.html',
+  styleUrls: ['./address-selector.component.css']
+})
+export class AddressSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
