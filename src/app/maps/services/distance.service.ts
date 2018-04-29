717d044f6a3d585046f5b8f7803b765d3ab5a3f1
Kim Kern <github@kimkern.de>
2018-04-29 19:06:44 +0200

    improve error handling

--- src/app/maps/services/distance.service.ts
+++ src/app/maps/services/distance.service.ts
@@ -3,13 +3,13 @@ import { Inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { DistanceEntry, DistanceMatrix } from '../../route-algorithms/distance-matrix';
 import { bindCallback } from 'rxjs/observable/bindCallback';
-import { map } from 'rxjs/operators';
-import { BoundCallbackObservable } from 'rxjs/observable/BoundCallbackObservable';
+import { catchError, map } from 'rxjs/operators';
 import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
 import { MatDialog, MatSnackBar } from '@angular/material';
 import { ConfirmDialogComponent } from '../../util/confirm-dialog/confirm-dialog.component';
 import { DestinationService } from './destination.service';
 import { GOOGLE } from '../maps.module';
+import { of } from 'rxjs/observable/of';
 
 /**
  * Calculates the distance of given destinations by pairs.
@@ -32,17 +32,16 @@ export class DistanceService implements Resolve<DistanceMatrix> {
    * This resolves, when the distance are fetched from the Google API.
    * @param {ActivatedRouteSnapshot} route
    * @param {RouterStateSnapshot} state
-   * @returns {Observable<DistanceMatrix> | DistanceMatrix}
+   * @returns {Observable<DistanceMatrix>}
    */
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<DistanceMatrix> | DistanceMatrix {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<DistanceMatrix> {
     const destinations = this.destinationService.getDestinationNames();
-    if (this.hasEnoughDestinations(destinations)) {
-      const matrix$ = this.getDistance(destinations);
-      matrix$.subscribe(m => !m.hasRoute ? this.showError(m) : null);
-      return matrix$;
-    } else {
+    if (!this.hasEnoughDestinations(destinations)) {
       this.router.navigate(['destinations']);
     }
+    return this.getDistance(destinations).pipe(
+      catchError(error => this.showError(error))
+    );
   }
 
   /**
@@ -52,27 +51,41 @@ export class DistanceService implements Resolve<DistanceMatrix> {
    */
   private getDistance(places: string[]): Observable<DistanceMatrix> {
     const destinations = this.destinationService.getDestinations();
-    const getDistance: any = bindCallback(this.distanceService.getDistanceMatrix.bind(this.distanceService), res => res);
-    const result: BoundCallbackObservable<any> = getDistance({origins: places, destinations: places, travelMode: 'DRIVING'});
-    return result.pipe(map(res => new DistanceMatrix(res, destinations)));
+    const getDistance: ((v) => Observable<any>) = bindCallback(this.distanceService.getDistanceMatrix);
+    const result = getDistance({origins: places, destinations: places, travelMode: 'DRIVING'});
+    return result.pipe(
+      map(res => {
+        if (res[1] === 'OK') {
+          return new DistanceMatrix(res[0], destinations);
+        } else {
+          throw new QueryLimitError(res[1]);
+        }
+      }),
+      map(matrix => {
+        const nonReachable: DistanceEntry[] = matrix.distanceEntries.filter(e => !e.isReachable);
+        if (nonReachable && nonReachable.length > 0) {
+          throw new NotReachableError(nonReachable[0].fromIndex, nonReachable[0].toIndex);
+        } else {
+          return matrix;
+        }
+      })
+    );
   }
 
   /**
-   * When there is no possible connection between at least one pair,
-   * an error is shown.
-   * @param {DistanceMatrix} matrix
+   * Display errors to user.
    */
-  private showError(matrix: DistanceMatrix) {
-    const destinations = this.destinationService.getDestinationNames();
-    const nonReachable: DistanceEntry[] = matrix.distanceEntries.filter(e => !e.isReachable);
+  private showError(error: QueryLimitError | NotReachableError) {
     this.router.navigate(['destinations']);
-    if (nonReachable && nonReachable.length > 0) {
-      const destA = destinations[nonReachable[0].fromIndex];
-      const destB = destinations[nonReachable[0].toIndex];
+    if (error instanceof NotReachableError) {
+      const destinations = this.destinationService.getDestinationNames();
+      const destA = destinations[error.fromIndex];
+      const destB = destinations[error.toIndex];
       this.showDialog(`No possible route between '${destA}' and '${destB}'.\nRemove one of them.`);
     } else {
-      this.showDialog('Too many requests.\nRetry in a couple of seconds.');
+      this.showDialog('API limit reached.\nRetry in a couple of seconds.');
     }
+    return of(null);
   }
 
   /**
@@ -94,5 +107,32 @@ export class DistanceService implements Resolve<DistanceMatrix> {
   private showDialog(message: string) {
     const config = {data: {title: 'Oops...', text: message, routeName: 'OK'}};
     this.dialog.open(ConfirmDialogComponent, config);
+  }
+}
+
+class QueryLimitError extends Error {
+  // workaround for instanceof checks, see https://goo.gl/sgSAMm
+  __proto__: QueryLimitError;
+
+  constructor(reason) {
+    const trueProto = new.target.prototype;
+    super(reason);
+    this.__proto__ = trueProto;
+
+    console.warn('reached query limit', this);
+  }
+}
+
+class NotReachableError extends Error {
+  __proto__: NotReachableError;
+  fromIndex;
+  toIndex;
+
+  constructor(fromIndex: number, toIndex: number) {
+    const trueProto = new.target.prototype;
+    super();
+    this.fromIndex = fromIndex;
+    this.toIndex = toIndex;
+    this.__proto__ = trueProto;
   }
 }
